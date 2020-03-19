81af13899425d605ac0064900fafe3e8ae14250b
okFox <smayfield@gmail.com>
2020-03-18 20:25:55 -0700

    finished

--- form/script.js
+++ form/script.js
@@ -15,7 +15,7 @@ const showSuccess = (input) => {
   formControl.className = 'form-control success';
 }
 
-const isValidEmail = () => {
+const checkEmail = (input) => {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
