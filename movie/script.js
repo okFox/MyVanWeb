23f9a3174c424e380b6d94b8aa96d83110e7dd06
okFox <smayfield@gmail.com>
2020-03-21 20:02:56 -0700

    local storage added for selections

--- movie/script.js
+++ movie/script.js
@@ -4,9 +4,18 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value; //querySelectorAll stores as node list
+let setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
 
 updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -14,13 +23,13 @@ updateSelectedCount = () => {
 
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value)
   updateSelectedCount();
 })
 
 container.addEventListener('click', (e) => {
   if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
     e.target.classList.toggle('selected');
-
     updateSelectedCount();
   }
 });
