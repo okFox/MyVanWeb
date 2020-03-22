7fc5bbcc99ae3e8994b248da690c53f56a2ca2f3
okFox <smayfield@gmail.com>
2020-03-21 20:16:43 -0700

    added populateUI function

--- movie/script.js
+++ movie/script.js
@@ -3,7 +3,12 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+
+populateUI();
+
 let ticketPrice = +movieSelect.value; //querySelectorAll stores as node list
+
+
 let setMovieData = (movieIndex, moviePrice) => {
   localStorage.setItem('selectedMovieIndex', movieIndex);
   localStorage.setItem('selectedMoviePrice', moviePrice)
@@ -21,6 +26,23 @@ updateSelectedCount = () => {
   total.innerText = selectedSeatsCount * ticketPrice;
 };
 
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
   setMovieData(e.target.selectedIndex, e.target.value)
@@ -34,3 +56,4 @@ container.addEventListener('click', (e) => {
   }
 });
 
+updateSelectedCount();
